1b92a8cd24a619dca4a7ec9fa949fdd5c8a07bdc
Nikolay <nik_mas@mail.ru>
2020-04-25 00:21:37 +0300

    commit message

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let money=+prompt("Ваш бюджет на месяц?","");
-let time=prompt("Введите дату в формате YYYY-MM-DD","");
-let appData={
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+let appData = {
     moneyData: money,
     timeData: time,
     expenses: {},
@@ -8,12 +8,12 @@ let appData={
     income: [],
     savings: false
 };
-let a=prompt("Введите обязательную статью расходов в этом месяце");
-let b=prompt("Во сколько обойдется?","");
-appData.expenses[a]=b;
+let a = prompt("Введите обязательную статью расходов в этом месяце");
+let b = prompt("Во сколько обойдется?","");
+appData.expenses[a] = b;
 console.log(appData);
 a=prompt("Введите обязательную статью расходов в этом месяце");
 b=prompt("Во сколько обойдется?","");
-appData.expenses[a]=b;
+appData.expenses[a] = b;
 console.log(appData);
 alert("Бюджет пользователя на 1 день " + money/30);
